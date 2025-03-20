c42659a5460f0a2df31b718ed439f87f07585d69
nicolasque <quequecedo@gmail.com>
2025-03-20 21:32:27 +0100

    Add multiple map files and error handling for various scenarios

--- src/parseo/get_file.c
+++ src/parseo/get_file.c
@@ -6,7 +6,7 @@
 /*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 21:07:43 by nquecedo          #+#    #+#             */
-/*   Updated: 2025/03/20 21:27:43 by nquecedo         ###   ########.fr       */
+/*   Updated: 2025/03/20 21:30:05 by nquecedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@ int	ft_get_file_data(int argc, char **argv, t_args *t_args)
 		return (printf("%sArguments, error \n%s", RED, RESET), 1);
 	argv++;
 	t_args->file_name = *argv;
+	printf("File name: %s\n",t_args->file_name );
 	return (0);
 }
